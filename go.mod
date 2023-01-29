668e77d346ea7c1a270cd4f4b40a58301d6ebc68
Henry Osei <rocahenry@gmail.com>
2023-01-29 22:27:57 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/henryosei/gop2p
+
+go 1.19
+
+require github.com/jackpal/bencode-go v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/jackpal/bencode-go"
+	"io"
+)
+
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+func OpenFile(reader io.Reader) (*bencodeTorrent, error) {
+	bto := bencodeTorrent{}
+	err := bencode.Unmarshal(r, &bto)
+	if err != nil {
+		return nil, err
+	}
+	return &bto, nil
+}
